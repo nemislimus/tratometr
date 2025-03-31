f42a979830d6698e62beb5ca029aa729807c92c6
Pavel_Davydov <david20506@yandex.ru>
2025-03-31 17:06:18 +0500

    Запрос регистрации. Работает

--- app/src/main/java/com/nemislimus/tratometr/MainActivity.kt
+++ app/src/main/java/com/nemislimus/tratometr/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.nemislimus.tratometr
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.nemislimus.tratometr.authorization.data.RetrofitNetworkClient
+import com.nemislimus.tratometr.authorization.data.dto.RegistrationRequest
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val message = findViewById<TextView>(R.id.message)
+
+        val networkClient = RetrofitNetworkClient()
+
+        val registrationRequest = RegistrationRequest("forothen@gmail.com", "12345678")
+
+        lifecycleScope.launch {
+            val response = networkClient.doRequest(registrationRequest)
+            message.text = response.resultCode.toString()
+        }
     }
 }
--- app/src/main/java/com/nemislimus/tratometr/authorization/data/RetrofitNetworkClient.kt
+++ app/src/main/java/com/nemislimus/tratometr/authorization/data/RetrofitNetworkClient.kt
@@ -1,10 +1,15 @@
 package com.nemislimus.tratometr.authorization.data
 
+import android.util.Log
+import com.nemislimus.tratometr.authorization.data.dto.AuthResponse
+import com.nemislimus.tratometr.authorization.data.dto.RegistrationRequest
+import com.nemislimus.tratometr.authorization.data.dto.Response
 import com.nemislimus.tratometr.authorization.data.network.ApiService
+import com.nemislimus.tratometr.authorization.data.network.NetworkClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitNetworkClient {
+class RetrofitNetworkClient : NetworkClient {
     private val baseUrl = "http://130.193.44.66:8080/"
 
     private val retrofit = Retrofit.Builder()
@@ -12,5 +17,43 @@ class RetrofitNetworkClient {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val service = retrofit.create(ApiService::class.java)
+    val service: ApiService = retrofit.create(ApiService::class.java)
+
+    override suspend fun doRequest(dto: Any): Response {
+        when (dto) {
+            is RegistrationRequest -> {
+                return doRegistrationRequest(dto)
+            }
+
+            /*is LoginRequest -> {
+
+            }
+
+            is RefreshTokenRequest -> {
+
+            }
+
+            is CheckTokenRequest -> {
+
+            }*/
+            else -> {
+                return Response().apply { resultCode = 600 }
+            }
+        }
+    }
+
+    private suspend fun doRegistrationRequest(dto: RegistrationRequest): AuthResponse {
+        try {
+            val response = service.register(dto)
+            if (response.isSuccessful){
+                return response.body()!!
+            } else {
+                Log.d("Ошибка2", response.toString())
+                return AuthResponse("","", 0).apply { resultCode = response.code() }
+            }
+        } catch (e: Exception) {
+            Log.d("Ошибка1", e.toString())
+            return AuthResponse("", "", 0).apply { resultCode = 1 }
+        }
+    }
 }
