e05c3e8b45399159289cef261545b00313899afb
nemislimus <nemislimus@yandex.ru>
2025-04-03 16:51:53 +0300

    #3 Добавил safeArgs плагин

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.kotlin.safeargs)
 }
 
 android {
