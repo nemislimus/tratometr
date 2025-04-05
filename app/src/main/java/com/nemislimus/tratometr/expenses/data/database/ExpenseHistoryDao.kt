520b39b44fb80339f26ae3a0b19a7e0ac462012f
A2T-user <luidor-arb@yandex.ru>
2025-04-06 01:14:34 +0400

    Создание запросов для окна Аналитики

--- app/src/main/java/com/nemislimus/tratometr/expenses/data/database/ExpenseHistoryDao.kt
+++ app/src/main/java/com/nemislimus/tratometr/expenses/data/database/ExpenseHistoryDao.kt
@@ -122,6 +122,47 @@ class ExpenseHistoryDao @Inject constructor(
     }
 
 // ################   ЗАПРОСЫ ДЛЯ ОКНА ВЫБОР КАТЕГОРИИ   ########################################################################################
+    // Список категорий с иконками и фильтром(период)
+    /*  Образец запроса
+        SELECT EXPENSES.CATEGORY, CATEGORIES.ICON_RES_ID
+        FROM EXPENSES LEFT JOIN CATEGORIES ON EXPENSES.CATEGORY = CATEGORIES.CATEGORY_NAME
+        WHERE (((EXPENSES.Date)>=500 And (EXPENSES.Date)<1555))
+        ORDER BY EXPENSES.CATEGORY;
+    */
+    fun getCategoriesListWithIconsRange(startDate: Long?, endDate: Long?): List<CategoryEntity> {
+        val db = databaseHelper.readableDatabase
+        val categories = mutableListOf<CategoryEntity>()
+        // Начинаем строить базовый запрос
+        val queryBuilder =
+            StringBuilder("SELECT EXPENSES.CATEGORY, CATEGORIES.ICON_RES_ID ")
+        queryBuilder.append("FROM EXPENSES LEFT JOIN CATEGORIES ON EXPENSES.CATEGORY = CATEGORIES.CATEGORY_NAME ")
+        queryBuilder.append("WHERE  1=1")
+        val args = mutableListOf<String>()
+        // Добавляем условия для дат, если они заданы
+        if (startDate != null) {
+            queryBuilder.append(" AND EXPENSES.DATE >= ?")
+            args.add(startDate.toString())
+        }
+        if (endDate != null) {
+            queryBuilder.append(" AND EXPENSES.DATE < ?")
+            args.add(endDate.toString())
+        }
+        // Добавляем сортировку
+        queryBuilder.append(" ORDER BY EXPENSES.CATEGORY;")
+        // Выполняем запрос
+        val cursor = db.rawQuery(queryBuilder.toString(), args.toTypedArray())
+
+        if (cursor.moveToFirst()) {
+            do {
+                val categoryName = cursor.getString(0)
+                val iconResId = cursor.getInt(1)
+                categories.add(CategoryEntity(categoryName, iconResId))
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        db.close()
+        return categories
+    }
 
 // ################   ЗАПРОСЫ ДЛЯ ОКНА СОЗДАНИЕ КАТЕГОРИИ   #####################################################################################
 
