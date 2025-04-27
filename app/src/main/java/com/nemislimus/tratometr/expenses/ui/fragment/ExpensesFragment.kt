bda876eb27f0abf889f2e058b3d72700dabb3686
A2T-user <luidor-arb@yandex.ru>
2025-04-27 19:46:09 +0400

    Удаление неиспользуемых ресурсов

--- app/src/main/java/com/nemislimus/tratometr/expenses/ui/fragment/ExpensesFragment.kt
+++ app/src/main/java/com/nemislimus/tratometr/expenses/ui/fragment/ExpensesFragment.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
