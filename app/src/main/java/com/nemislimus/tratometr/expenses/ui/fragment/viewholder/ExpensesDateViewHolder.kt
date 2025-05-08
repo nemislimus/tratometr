c11aedfba627b5691deaf03cdc218281767d2fd6
A2T-user <luidor-arb@yandex.ru>
2025-05-08 11:21:30 +0400

    Правильное отбражение даты в Списке расходов

--- app/src/main/java/com/nemislimus/tratometr/expenses/ui/fragment/viewholder/ExpensesDateViewHolder.kt
+++ app/src/main/java/com/nemislimus/tratometr/expenses/ui/fragment/viewholder/ExpensesDateViewHolder.kt
@@ -11,13 +11,20 @@ import java.util.Locale
 class ExpensesDateViewHolder(private val binding: ItemHistoryDateBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: Historical.HistoryDate) {
-        val dateFormat = SimpleDateFormat("dd.MM.yy", Locale.getDefault())
+        val dateFormat = SimpleDateFormat("d MMMM yyyy", Locale.getDefault())
         val currentDate = Date()
+        val currentYear = SimpleDateFormat("yyyy", Locale.getDefault()).format(currentDate)
         val today = dateFormat.format(currentDate)
         if (item.date == today) {
             binding.tvDate.setText(R.string.today)
         } else {
-            binding.tvDate.text = item.date
+            binding.tvDate.text = getDateString(item.date, currentYear)
+        }
+    }
+
+    private fun getDateString(date: String, currentYear: String): String {
+        return date.split(" ").let { parts ->
+            if (parts[2] == currentYear) "${parts[0]} ${parts[1]}" else "${parts[0]} ${parts[1]} ${parts[2]}"
         }
     }
 }
